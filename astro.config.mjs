484a607b689d83d07cadf6369adb6ef6433b14b2
Asis Unyapoth <asisunyapoth@Asiss-MacBook-Pro.local>
2024-08-15 18:30:13 +0700

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'DGA Docs',
+    customCss: ['@fontsource/noto-sans-thai/400.css', '@fontsource/noto-sans-thai/600.css'],
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Citizen Portal',
+      autogenerate: {
+        directory: 'czp'
+      }
+    }, {
+      label: 'GDX',
+      autogenerate: {
+        directory: 'gdx'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: netlify()
+});
---
+++ src/content/docs/edoc/_index.md
@@ -0,0 +1,6 @@
+---
+Title: e-Document
+weight: 30
+date: 2024-03-04T08:58:05.157Z
+---
+การออกเอกสารหลักฐานทางราชการผ่านระบบดิจิทัล (Electronic Document)
